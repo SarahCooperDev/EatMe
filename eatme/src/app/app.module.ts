9f9bbeb90de938a87fd396d3dc6aed6114468a16
ImmerSie <31569454+ImmerSie@users.noreply.github.com>
2018-08-27 23:18:46 +1000

    Minor UI tweak, committing to merge

--- eatme/src/app/app.module.ts
+++ eatme/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
