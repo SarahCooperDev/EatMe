06098fc81620060232e7388ecfd6206f571bb0a0
Madeleine <madeleine.gillard@gmail.com>
2018-10-10 21:31:30 +1100

    server.js env

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(session({
 }));
 
 app.use(function(req, res, next){
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'http://'+HOSTURL+':4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', "Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,authorization,rbr");
   res.setHeader('Access-Control-Allow-Credentials', true);
