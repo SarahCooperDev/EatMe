d27c9d3476b474142bbbde3ddb78f73f0ea92c72
Madeleine <madeleine.gillard@gmail.com>
2018-10-11 16:02:44 +1100

    menu in settings and server fixed

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(session({
 }));
 
 app.use(function(req, res, next){
-  res.setHeader('Access-Control-Allow-Origin', 'http://http://13.211.135.245/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://13.211.135.245');
   //res.setHeader('Access-Control-Allow-Origin', 'http://'+'localhost'+':4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', "Access-Control-Allow-Headers,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,authorization,rbr");
