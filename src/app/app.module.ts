0bb0cfa3db97ff63b45f2171733375dd1d1a453d
Madeleine <madeleine.gillard@gmail.com>
2018-10-17 19:01:57 +1100

    missed a file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AuthComponent } from './auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SettingsComponent } from './settings/settings.component';
-import { ListComponent } from './list/list.component';
-import { PlaceDetailComponent } from './place-detail/place-detail.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
@@ -37,8 +35,6 @@ import {MatListModule} from '@angular/material/list';
     AuthComponent,
     DashboardComponent,
     SettingsComponent,
-    ListComponent,
-    PlaceDetailComponent,
     ArchiveComponent,
     ImageUploadComponent,
     ArchiveImagesComponent,
